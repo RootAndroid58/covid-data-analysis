738e7d4f583b8a254ee8b30e2eb200dfa59ac976
RootAndroid58 <rootand58@gmail.com>
2021-05-16 21:07:19 +0530

    updated code

--- resources/views/partials/menu.blade.php
+++ resources/views/partials/menu.blade.php
@@ -1,7 +1,7 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4" style="min-height: 917px;">
     <!-- Brand Logo -->
     <a href="#" class="brand-link">
-        <img src="{{ asset('/img/favicon-96x96.png') }}" width="50" alt="img" srcset="">
+        <img src="{{ asset('/image/favicon-96x96.png') }}" width="50" alt="img" srcset="">
         <span class="brand-text font-weight-light">{{ trans('panel.site_title') }}</span>
     </a>
 
