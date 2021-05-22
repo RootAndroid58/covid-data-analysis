a7857b1f8281d7c9127febbc17cb5f113603822e
RootAndroid58 <rootand58@gmail.com>
2021-05-22 12:02:22 +0530

    updated logo

--- resources/views/partials/menu.blade.php
+++ resources/views/partials/menu.blade.php
@@ -1,7 +1,7 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4" style="min-height: 917px;">
     <!-- Brand Logo -->
     <a href="#" class="brand-link">
-        <img src="{{ asset('/image/favicon-96x96.png') }}" width="50" alt="img" srcset="">
+        <img src="{{ asset('/image/favcon-500x500.jpeg') }}" width="50" alt="img" srcset="">
         <span class="brand-text font-weight-light">{{ trans('panel.site_title') }}</span>
     </a>
 
