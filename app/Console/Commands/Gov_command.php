f8a7b9c6b1e3bbca6d1eb20faa703e779af87f14
RootAndroid58 <rootand58@gmail.com>
2021-06-15 21:16:23 +0530

    added schedules

--- app/Console/Commands/Gov_command.php
+++ app/Console/Commands/Gov_command.php
@@ -44,44 +44,44 @@ class Gov_command extends Command
         $this->info('covid:gov-canada');
         $this->call('covid:gov-canada');
 
-        // $this->info('covid:gov-colombia');
-        // $this->call('covid:gov-colombia');
+        $this->info('covid:gov-colombia');
+        $this->call('covid:gov-colombia');
 
-        // $this->info('covid:gov-germany');
-        // $this->call('covid:gov-germany');
+        $this->info('covid:gov-germany');
+        $this->call('covid:gov-germany');
 
-        // $this->info('covid:gov-india');
-        // $this->call('covid:gov-india');
+        $this->info('covid:gov-india');
+        $this->call('covid:gov-india');
 
-        // $this->info('covid:gov-indo');
-        // $this->call('covid:gov-indo');
+        $this->info('covid:gov-indo');
+        $this->call('covid:gov-indo');
 
-        // $this->info('covid:gov-israel');
-        // $this->call('covid:gov-israel');
+        $this->info('covid:gov-israel');
+        $this->call('covid:gov-israel');
 
-        // $this->info('covid:gov-italy');
-        // $this->call('covid:gov-italy');
+        $this->info('covid:gov-italy');
+        $this->call('covid:gov-italy');
 
-        // $this->info('covid:gov-nz');
-        // $this->call('covid:gov-nz');
+        $this->info('covid:gov-nz');
+        $this->call('covid:gov-nz');
 
-        // $this->info('covid:gov-nigeria');
-        // $this->call('covid:gov-nigeria');
+        $this->info('covid:gov-nigeria');
+        $this->call('covid:gov-nigeria');
 
-        // $this->info('covid:gov-southafrica');
-        // $this->call('covid:gov-southafrica');
+        $this->info('covid:gov-southafrica');
+        $this->call('covid:gov-southafrica');
 
-        // $this->info('covid:gov-southkorea');
-        // $this->call('covid:gov-southkorea');
+        $this->info('covid:gov-southkorea');
+        $this->call('covid:gov-southkorea');
 
-        // $this->info('covid:gov-switzerland');
-        // $this->call('covid:gov-switzerland');
+        $this->info('covid:gov-switzerland');
+        $this->call('covid:gov-switzerland');
 
-        // $this->info('covid:gov-uk');
-        // $this->call('covid:gov-uk');
+        $this->info('covid:gov-uk');
+        $this->call('covid:gov-uk');
 
-        // $this->info('covid:gov-vietnam');
-        // $this->call('covid:gov-vietnam');
+        $this->info('covid:gov-vietnam');
+        $this->call('covid:gov-vietnam');
 
         return 0;
     }
